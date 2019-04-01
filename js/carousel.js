1c05b3f7a147d2c0466c3dd54c2e2c2aa68f1311
Xunchao <xchu87@gmail.com>
2019-04-01 13:38:03 -0700

    intial change

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.main-carousel').slick({
+	  adaptiveHeight: false,
+	  autoplay: true,
+	  arrows: false,
+	  fade: true,
+	  autoplaySpeed: 3000,
+	  dots: true,
+	  adaptiveHeight: false
+	  });
+});
+
